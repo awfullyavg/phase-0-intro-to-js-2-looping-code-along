374c6f93e3431f266c8e37d76afa94480fa9b50f
Bryant Wiggins <topwigit@gmail.com>
2023-04-19 16:00:33 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(input) {
+    const cards = [];
+    for (let i = 0; i < input.length; i++) {
+      const name = input[i]
+      let message = `Thank you, ${name}, for the wonderful surprise gift!`
+      cards.push(message);
+    }
+    return cards;
+  }
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
